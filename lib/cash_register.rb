6eee34e687859153faa37a87c1ca710fa201a182
thyoon515 <thyoon515@gmail.com>
2022-10-12 22:46:11 -0400

    commit tree 257c5deb63092f57c0b1c2e42055c9525ebee194

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
+class CashRegister
+
+    attr_accessor :total, :discount
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+    end
+
+    def apply_discount
+        self.total = self.total - discount / 100.0 * self.total
+        puts "After the discount, the total comes to $#{self.total}."
+    end
+end
 
